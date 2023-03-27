fd09584f1bee6e196a40e8b364eabe5633b20abc
Abhishek singh chauhan <115978151+abhishek-06-singh@users.noreply.github.com>
2023-03-27 11:41:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from "react";
-import "./App.css";
+import "./app.css";
 import "./controls.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
